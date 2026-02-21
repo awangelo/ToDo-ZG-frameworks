eda0a5c20623744e0a8e785ae2f837c31dd04074
awangelo <anggg.gaio@gmail.com>
2026-02-21 20:31:24 -0300

    feat: Add routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/tarefas', pathMatch: 'full' },
+  { path: 'tarefas', component: HomeComponent },
+  // { path: 'tarefas/nova', component: TaskFormComponent },
+  // { path: 'tarefas/editar/:id', component: TaskFormComponent },
+  // { path: 'tarefas/:id', component: TaskDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

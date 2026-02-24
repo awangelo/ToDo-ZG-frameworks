3bddda6899ac586c5267a2134c5cdc382a4a6959
awangelo <anggg.gaio@gmail.com>
2026-02-24 17:38:46 -0300

    feat: Implement tarefa listing on sections

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tarefa, Status } from '../models';
+import { TarefaService } from '../services/tarefa.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  Status = Status;
+  tarefas: Tarefa[] = [];
 
+  constructor(private tarefaService: TarefaService) {}
+
+  ngOnInit(): void {
+    this.carregarTarefas();
+  }
+
+  carregarTarefas(): void {
+    this.tarefaService.listar().subscribe({
+      next: (tarefas) => this.tarefas = tarefas,
+      error: (err) => console.error('Erro ao carregar tarefas:', err)
+    });
+  }
+
+  get todoTarefas(): Tarefa[] {
+    return this.tarefas.filter(t => t.status === Status.TODO);
+  }
+
+  get doingTarefas(): Tarefa[] {
+    return this.tarefas.filter(t => t.status === Status.DOING);
+  }
+
+  get doneTarefas(): Tarefa[] {
+    return this.tarefas.filter(t => t.status === Status.DONE);
+  }
 }

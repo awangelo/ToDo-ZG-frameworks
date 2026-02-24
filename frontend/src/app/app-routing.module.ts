db31ed1c52746aed13f03eb4f6a09b09b9b82001
awangelo <anggg.gaio@gmail.com>
2026-02-24 17:57:36 -0300

    fix: Change routes to use /home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/tarefas', pathMatch: 'full' },
-  { path: 'tarefas', component: HomeComponent },
-  // { path: 'tarefas/nova', component: TaskFormComponent },
-  // { path: 'tarefas/editar/:id', component: TaskFormComponent },
-  // { path: 'tarefas/:id', component: TaskDetailsComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'home/nova', component: TaskFormComponent },
+  { path: 'home/editar/:id', component: TaskFormComponent },
+  { path: 'home/:id', component: TaskDetailsComponent }
 ];
 
 @NgModule({
